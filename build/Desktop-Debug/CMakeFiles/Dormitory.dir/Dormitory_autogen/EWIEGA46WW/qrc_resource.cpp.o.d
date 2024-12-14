47c916bc3fdc14e9c6213bd4e7d6a0c66527bf96
Sergey <144939891+FroZzeLs@users.noreply.github.com>
2024-12-14 21:24:56 +0300

    Delete build/Desktop-Debug directory

--- build/Desktop-Debug/CMakeFiles/Dormitory.dir/Dormitory_autogen/EWIEGA46WW/qrc_resource.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Dormitory.dir/Dormitory_autogen/EWIEGA46WW/qrc_resource.cpp.o: \
- /home/sergey/Dormitory/build/Desktop-Debug/Dormitory_autogen/EWIEGA46WW/qrc_resource.cpp \
- /usr/include/stdc-predef.h
